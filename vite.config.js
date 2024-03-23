3ce010aaf99e4f5ca4bcb208bd222e78021084c1
Asep Suprapto <asepsuprapto.id@gmail.com>
2024-03-23 20:52:41 +0700

    vite config for build

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,21 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  base: "./",
+  build: {
+    chunkSizeWarningLimit: 600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
